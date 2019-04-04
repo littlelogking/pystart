7d7df31df05e06ab71a0585479b88505513393df
littlelogking <louisg0709@gmail.com>
2019-04-04 21:34:40 +0100

    added more stuff to image viewer

--- Image-Viewer.py
+++ Image-Viewer.py
@@ -12,26 +12,61 @@ import matplotlib.pyplot as plt
 import matplotlib.image as mping
 import numpy as np
 
-#matplotlib.use('TkAgg')
+#matplotlib.use('TkAgg'
 
-
-
-
-def showimage(imagefile):
+#TODO 
+def showimage1(imagefile):
+    #print('hell from image1')
     status = 6  
     img=mping.imread(imagefile)
-    print (status+1)
+    #print (status+1)
     plt.imshow(img)
     plt.show()
-    print (status)
+    #print (status)
     return status
-              
+
+def showimage2(imagefile):
+    status = 6  
+    img=mping.imread(imagefile)
+    #print (status+1)
+    plt.imshow(img)
+    plt.show()
+    #print (status)
+    return status
+
+def showimage3(imagefile):
+    status = 6  
+    img=mping.imread(imagefile)
+    #print (status+1)
+    plt.imshow(img)
+    plt.show()
+    #print (status)
+    return status
+
+#main routine starts here
+    
+print ('Which Image would you like to view Image 1, 2 or 3: ')
+image = int(input('1, 2, or 3'))
+#print(image)
+if image == 1:
+    showimage1('images/image1.jpg')
+
+    
+if image == 2:
+    showimage2('images/image2.jpg')
+
+    
+if image == 3:
+    showimage3('images/image3.png')
 
 
-print(matplotlib.get_backend())
+
+
+
+
 #%matplotlib inline
 
-showimage('images/image.jpg')
+#showimage1('images/image.jpg')
 
 #img=mping.imread('images/image.jpg')
 #img.view()
