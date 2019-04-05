8ecbad1701a0d64037ab74f6a319a18043b2f350
littlelogking <louisg0709@gmail.com>
2019-04-05 21:40:41 +0100

    Practised using functions found fatal errors

--- Image-Viewer.py
+++ Image-Viewer.py
@@ -11,11 +11,15 @@ import matplotlib
 import matplotlib.pyplot as plt
 import matplotlib.image as mping
 import numpy as np
+import sys
+
+
+
 
 #matplotlib.use('TkAgg'
 
 #TODO 
-def showimage1(imagefile):
+def showimage(imagefile):
     #print('hell from image1')
     status = 6  
     img=mping.imread(imagefile)
@@ -25,39 +29,36 @@ def showimage1(imagefile):
     #print (status)
     return status
 
-def showimage2(imagefile):
-    status = 6  
-    img=mping.imread(imagefile)
-    #print (status+1)
-    plt.imshow(img)
-    plt.show()
-    #print (status)
+def stop():
+    status=0
+    print("Goodbye")
+    sys.exit(0)
+    
     return status
+    
 
-def showimage3(imagefile):
-    status = 6  
-    img=mping.imread(imagefile)
-    #print (status+1)
-    plt.imshow(img)
-    plt.show()
-    #print (status)
-    return status
 
 #main routine starts here
     
 print ('Which Image would you like to view Image 1, 2 or 3: ')
-image = int(input('1, 2, or 3'))
+image = (input('1, 2, 3 or none'))
 #print(image)
-if image == 1:
-    showimage1('images/image1.jpg')
+if image == '1':
+    showimage('images/image1.jpg')
+    stop()
 
     
-if image == 2:
-    showimage2('images/image2.jpg')
+if image == '2':
+    showimage('images/image2.jpg')
+    stop()
 
     
-if image == 3:
-    showimage3('images/image3.png')
+if image == '3':
+    showimage('images/image3.png')
+    stop()
+    
+
+print ("which file in pystart is your image in")
 
 
 
--- ghostgametest_2.py
+++ ghostgametest_2.py
@@ -9,20 +9,38 @@ Created on Sun Jul 26 08:43:51 2015
 # Ghost Game
 
 from random import randint
+import sys
+
+#FIXME
+
 
 def scoref():
     result = score
     return result
+
+def ghostdoor(score):
+      print( 'GHOST!')
+      feeling_brave = False
+      print('You enter the next room.')
+      print('Game over! You scored',score)
+      return feeling_brave
+      
+def safedoor(score):
+    print('No Ghost!')
+    score=score+1
+    print('Move on to the next room')
+    return score
+     
+    
 
     
 
 
 print('Ghost Game')
 
-feeling_brave = True
-
+#TODO work through logic here
 score = 0
-
+feeling_brave = True
 while feeling_brave:
     safe_door = randint(1, 10)   
     print('ten doors ahead...')    
@@ -30,26 +48,17 @@ while feeling_brave:
     print('which door do you open?'  )
     door = input( ' 1, 2, 4, 5, 6, 7, 8, 9, 10 or 3? '  ) 
     door_num = int(door) 
-    if door_num < safe_door:
-        print( 'GHOST!')
-        feeling_brave = True
-        print('You enter the next room.')
-        print('Game over! You scored',score)
-        score = 0
-    else:
-        print('No Ghost!')
-        score=score+1
-        print('Move on to the next room')
-        
-    print (scoref())
-      
+    
     if door_num > safe_door:
-        print( 'GHOST!')
-        feeling_brave = False
-        print('Game over! You scored',score)
-        score = 0
+        ghostdoor(score)
+    
+    if door_num < safe_door:
+       ghostdoor(score)
+        
     else:
-        print('No Ghost!')
-        score=score+1
-        print('Move on to the next room')
+       score=safedoor(score)
+        
+      
+    
+        
  
